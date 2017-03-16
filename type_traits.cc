e622b2ce49e4411f59eec237c7547f21923a534c
yuyue <yuyue2200@hotmail.com>
2017-03-16 10:02:45 +0800

    Add

--- type_traits.cc
+++ type_traits.cc
@@ -13,42 +13,20 @@ struct integral_constant {
 
 template<typename T, T v>
 const T integral_constant<T, v>::value;
-
 typedef integral_constant<bool, true> true_value;
 typedef integral_constant<bool, false> false_value;
 
 // Meta programmming helper types
-template<bool Cond, typename Iftrue, typename Iffalse>
-struct Conditional { typedef Iftrue type; };
-
-template<typename Iftrue, typename Iffalse>
-struct Conditional<false, Iftrue, Iffalse> { typedef Iffalse type; };
-
-template<typename Tp>
-struct Not: public integral_constant<bool, !Tp::value> {};
-
-template<typename Tp>
-struct remove_const { typedef Tp type; };
-
-template<typename Tp>
-struct remove_const<const Tp> { typedef Tp type; };
-
-template<typename Tp>
-struct remove_volatile { typedef Tp type; };
-
-template<typename Tp>
-struct remove_volatile<volatile Tp> { typedef Tp type; };
-
-template<typename Tp>
-struct remove_cv {
-    typedef typename remove_const<typename remove_volatile<Tp>::type>::type type;
-};
-
-template<typename Tp>
-struct remove_reference { typedef Tp type; };
-
-template<typename Tp>
-struct remove_reference<Tp&> { typedef Tp type; };
+template<bool Cond, typename Iftrue, typename Iffalse> struct Conditional { typedef Iftrue type; };
+template<typename Iftrue, typename Iffalse> struct Conditional<false, Iftrue, Iffalse> { typedef Iffalse type; };
+template<typename Tp> struct Not: public integral_constant<bool, !Tp::value> {};
+template<typename Tp> struct remove_const { typedef Tp type; };
+template<typename Tp> struct remove_const<const Tp> { typedef Tp type; };
+template<typename Tp> struct remove_volatile { typedef Tp type; };
+template<typename Tp> struct remove_volatile<volatile Tp> { typedef Tp type; };
+template<typename Tp> struct remove_cv { typedef typename remove_const<typename remove_volatile<Tp>::type>::type type; };
+template<typename Tp> struct remove_reference { typedef Tp type; };
+template<typename Tp> struct remove_reference<Tp&> { typedef Tp type; };
 
 // is_void
 template<typename Tp> struct is_void_helper: public false_value {};
