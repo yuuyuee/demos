23953bd58882601b99714f25964c4d09a9ac349f
Yue Yu <yuyue2200@hotmail.com>
2022-06-16 10:22:56 +0800

    update

--- design_patterns/singleton.cc
+++ design_patterns/singleton.cc
@@ -1,58 +1,38 @@
-#include <cassert>
-#include <type_traits>
-#if defined(USE_PTHREAD)
-# include <pthread.h>
-#else
-# include <mutex>
-# include <thread>
-#endif
+#include <mutex>
 
 template <typename Tp>
 class Singleton {
-  static_assert(std::is_constructible<Tp>::value, "");
-
  public:
   static inline Tp* Instance();
 
  private:
   static inline void Init();
-#if defined(USE_PTHREAD)
-  static pthread_once_t flag;
-#else
   static std::once_flag flag;
-#endif
   static Tp* ptr;
 };
 
 template <typename Tp>
-#if defined(USE_PTHREAD)
-pthread_once_t Singleton<Tp>::flag = PTHREAD_ONCE_INIT;
-#else
 std::once_flag Singleton<Tp>::flag;
-#endif
 
 template <typename Tp>
 Tp* Singleton<Tp>::ptr = nullptr;
 
 template <typename Tp>
 void Singleton<Tp>::Init() {
-  assert(ptr == nullptr);
   ptr = new Tp;
 }
 
 template <typename Tp>
 Tp* Singleton<Tp>::Instance() {
-#if defined(USE_PTHREAD)
-  pthread_once(&flag, Init);
-#else
   std::call_once(flag, Init);
-#endif
   return ptr;
 }
 
 //////////////////////////////////////////////////////////////////////
+#include <thread>
 #include <iostream>
 int main() {
+  std::thread  t1([]{}); t1.join();
   *(Singleton<int>::Instance()) = 100;
   std::cout <<*(Singleton<int>::Instance()) << std::endl;
   return 0;
