8b780294a6e5f9d1ede4d8031b3c9759ec19f95c
Yue Yu <yuyue2200@hotmail.com>
2022-07-26 11:49:27 +0800

    update

--- oak/oak-devel/src/version.c
+++ oak/oak-devel/src/version.c
@@ -1,3 +1,4 @@
+// Copyright 2022 The Oak Authors.
 
 #include "oak/version.h"
 
--- oak/oak/restore.cc
+++ oak/oak/restore.cc
@@ -1,3 +1,4 @@
+// Copyright 2022 The Oak Authors.
 
 void LoadConfig();
 void CreateShareMemory();
@@ -6,27 +7,18 @@ void CreateWorkers();
 void WaitWrokersReady();
 void EventLoop();
 
-#define PRIMARY_THREAD 1
-#define SECONDARY_THREAD 2
-
-int GetThreadFlags();
-
 // can not creating multiple processes with boot many process, because
 // the number of processes is configured in configuration.
 
 int main(int argc, char* argv[]) {
-
-  int flag = GetThreadFlags();
-
   LoadConfig();
-  if (flag == PRIMARY_THREAD) {
-    CreateShareMemory();
-    CreateWorkers();
-    WaitWrokersReady();
-    EventLoop();
-  } else {
-    AssignShareMemory();
-  }
+
+  CreateShareMemory();
+  CreateWorkers();
+  WaitWrokersReady();
+  EventLoop();
+  AssignShareMemory();
+
 
 
   return 0;
